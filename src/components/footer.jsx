a9b132ea6671c1de41d2b7d544e7208162120e9d
DonJuan <armvnov1ch@gmail.com>
2025-09-08 21:25:28 +0500

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
 export default function Footer() {
-  return (
-    <div id="footer">
-      <h1>footer</h1>
-    </div>
-  );
+   return (
+      <section id="footer">
+         <div className="container">
+            <div className="logo">
+               <img src="img/logo.svg" alt="Equipe" />
+            </div>
+            <div className="footer">
+              <p>© 2025 Все права защищены.</p>
+              <p>Политика конфиденциальности</p>
+            </div>
+         </div>
+      </section>
+   );
 }

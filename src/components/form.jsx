2126fcbdd4b13ee55644f869408a02f9433275ba
DonJuan <armvnov1ch@gmail.com>
2025-09-14 17:29:41 +0500

    added form post request

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,18 +1,40 @@
 import React from "react";
+
 export default function ContactForm() {
    return (
       <div id="form">
          <div className="container">
             <h2>Заполните заявку, и мы свяжемся с вами в ближайшее время</h2>
 
-            <form>
+            <form 
+               action="https://getform.io/f/bvrmprzb" 
+               method="POST"
+            >
                <div>
-                  <input type="text" placeholder="Напишите ФИО" />
+                  <input 
+                     type="text" 
+                     name="name"
+                     placeholder="Напишите ФИО" 
+                     required 
+                  />
                </div>
                <div>
-                  <input type="text" placeholder="Напишите номер" />
+                  <input 
+                     type="text" 
+                     name="phone"
+                     placeholder="Напишите номер" 
+                     required 
+                  />
                </div>
-                  <button type="submit">Отправить</button>
+
+               {/* Honeypot для защиты от ботов */}
+               <input 
+                  type="hidden" 
+                  name="_gotcha" 
+                  style={{ display: "none" }} 
+               />
+
+               <button type="submit">Отправить</button>
             </form>
          </div>
       </div>
